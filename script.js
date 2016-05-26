d905037d3d877c051dce266ae887aae18daf963e
Casey Burnett <burnett.casey@gmail.com>
2016-05-25 18:50:18 -0600

    removed comments

--- script.js
+++ script.js
@@ -44,9 +44,6 @@ function letterCapitalize(str){
 letterCapitalize(myString);
 
 
-// Thoughts -- make string an array
-
-
 // Write a function called wordCount which takes a single string parameter and 
 // returns the number of words the string contains (ie. "Never eat shredded wheat" would return 4). 
 // You can assume that words in the input string will be separated by only one space.
