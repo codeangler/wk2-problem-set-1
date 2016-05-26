f8f4a83ffbab222e2d86274ac0672ece54433b31
Casey Burnett <burnett.casey@gmail.com>
2016-05-26 10:33:12 -0600

    complete letterCapitalize() & countWords() & resubmitted application optimusPrime()

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Write a function called letterCapitalize which takes a single string parameter and 
 // capitalizes the first letter of each word in the string. 
 // You can assume that words in the input string will be separated by only one space.
-var myString = "Hello today is your day!"
+var myString = "Hello today is your day!";
 //  Working output of passing the string into function that makes it into an array then passes
 // array into a for of loop and returns to console log first index itemt
 
@@ -20,7 +20,6 @@ var myString = "Hello today is your day!"
 // 	return up.toUpperCase()[0] + 
 // 	(function(up){
 // 		up.splice(2)
-
 // 		;});
 
 // Attempting make a function call
@@ -40,10 +39,38 @@ function letterCapitalize(str){
 	console.log(dopamine);
 }
 
-
 letterCapitalize(myString);
 
 
 // Write a function called wordCount which takes a single string parameter and 
 // returns the number of words the string contains (ie. "Never eat shredded wheat" would return 4). 
 // You can assume that words in the input string will be separated by only one space.
+
+function wordCount(str) {
+	var results = str.split(" ");
+	console.log((results.length));
+
+}
+
+// Bonus was completed for admission
+var userNumber = prompt("Enter a postive integer to check if it is a prime number");
+
+function optimusPrime(passedNumber) {
+  if (passedNumber == 0 || passedNumber == 1) {
+    return prime = passedNumber + " is not composite nor is it prime."
+  } else if (passedNumber == 2 || passedNumber == 3) {
+    return prime = passedNumber + " is prime."
+  }
+  var ns = Math.sqrt(passedNumber);
+  for (i = 2; i <= ns; i++) {
+    var remainder = passedNumber % i
+    if (remainder == 0) {
+      return isItPrime = passedNumber + " is not prime as " + i + " divides evenly into it."
+    }
+    isItPrime = passedNumber + " is prime."
+  }
+
+};
+optimusPrime(userNumber);
+var display = isItPrime
+console.log(display);
